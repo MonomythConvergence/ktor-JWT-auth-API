69fc557a3bb4cbba110b7c32180d435979eeb030
Monomythconvergence <shoulddoisuppose@gmail.com>
2025-09-30 23:11:32 +0300

    Added pepper to local.properties, next up is hashing

--- src/main/kotlin/domain/HashingInterfaceImpl.kt
+++ src/main/kotlin/domain/HashingInterfaceImpl.kt
@@ -2,5 +2,6 @@ package com.example.domain
 
 class HashingInterfaceImpl : HashingInterface {
     override fun hash(password: String): ByteArray {
+    return ByteArray(0) //todo add SHA-256 hashing + pepper
     }
 }
